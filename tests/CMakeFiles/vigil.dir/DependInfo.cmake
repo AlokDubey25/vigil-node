b7e7cab0699df323214b4d8c5535f17c43e1e3bc
Alok Dubey <just25alok@gmail.com>
2026-06-02 11:05:55 +0000

    Finally, finally 😭 the error is resolved : Data type error

---
+++ tests/CMakeFiles/vigil.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/JUST_ALOK/vigil-node/cpp/src/main.cpp" "CMakeFiles/vigil.dir/cpp/src/main.cpp.o" "gcc" "CMakeFiles/vigil.dir/cpp/src/main.cpp.o.d"
+  "/mnt/c/Users/JUST_ALOK/vigil-node/cpp/src/orderbook.cpp" "CMakeFiles/vigil.dir/cpp/src/orderbook.cpp.o" "gcc" "CMakeFiles/vigil.dir/cpp/src/orderbook.cpp.o.d"
+  "" "vigil" "gcc" "CMakeFiles/vigil.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/CMakeFiles/vigil.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vigil.dir/link.d"
+  "CMakeFiles/vigil.dir/cpp/src/main.cpp.o"
+  "CMakeFiles/vigil.dir/cpp/src/main.cpp.o.d"
+  "CMakeFiles/vigil.dir/cpp/src/orderbook.cpp.o"
+  "CMakeFiles/vigil.dir/cpp/src/orderbook.cpp.o.d"
+  "vigil"
+  "vigil.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vigil.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
