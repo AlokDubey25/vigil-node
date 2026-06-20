e69d57274da21ff297f4032e7da271c91047e68b
Alok Dubey <just25alok@gmail.com>
2026-06-20 17:00:20 +0000

    Today started with graph implemntation for fraud detection - graph.h implemeted...

---
+++ cpp/include/graph.h
@@ -0,0 +1,37 @@
+# pragma once
+# include <string>
+# include <unordered_map>
+# include <unordered_set>
+# include <vector>
+# include <iostream>
+# include <iomanip>
+using namespace std;
+
+class Graph {
+public:
+    // edge for buyes to trade with seller
+    void addEdge(const string& buyer, const string& seller);
+
+    // it will be true if user is in a trading cycle (like kinda wash trade)
+    bool isInCycle(const string& userID) const;
+
+    // for number of times unique counterparties this user has traded with
+    int getDegree(const string& userID) const;
+
+    // all users currently in a cycle
+    vector<string> getCircularTraders() const;
+
+    // debug - print full trade network
+    void print() const;
+
+
+private:
+    // adjust user - set of users has brought FROM
+    unordered_map<string, unordered_set<string>> adj_;
+
+    // DFS - we can get from current back to staet in <= maxDepth hops may be or may be not...
+    bool dfsCycle(const string& start, 
+                  const string& current,
+                  unordered_set<string>& visited,
+                  int depth, int maxDepth) const;
+}
